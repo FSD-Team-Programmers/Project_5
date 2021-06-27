b302a8a9d4b5d8f8249d3da82d00a40dd28b676d
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-06-27 23:44:27 +0300

    dashboard reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,4 +2,33 @@ const initialState = {
     places: [],
   };
   ​
+  const places = (state = initialState, {type, payload}) => {
+
+    switch (type) {
   
+      case 'ADD_PLACES':
+        return { places: [...state.places, payload] };
+  
+      case 'UPDATE_PLACES':
+        return {
+          places: state.places.map((place) => {
+            if (place._id === payload._id) {
+              return payload;
+            } else {
+              return place;
+            }
+          }),
+        };
+  
+      case 'DELETE_PLACES':
+        return {
+          places: state.places.filter(
+            (place) => place._id !== payload._id,
+          ),
+        };
+  
+      default:
+        return state;
+    }
+  };
+  export default places;
